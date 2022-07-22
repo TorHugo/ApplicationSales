380b35c7c9a924100497a950c89d2b0d813a74b4
Victor Hugo de Arruda Jesus <88560162+TorHugo@users.noreply.github.com>
2022-07-21 21:19:12 -0300

    Decodificando o Token (Jwt).

--- vendas/src/main/java/com/api/vendas/JwtService.java
+++ vendas/src/main/java/com/api/vendas/JwtService.java
@@ -1,15 +1,19 @@
 package com.api.vendas;
 
 import com.api.vendas.domain.entity.Usuario;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.xml.crypto.Data;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.HashMap;
 
 @Service
 public class JwtService {
@@ -32,5 +36,34 @@ public class JwtService {
                 .setExpiration(data)
                 .signWith( SignatureAlgorithm.HS512, chaveAssinatura )
                 .compact();
+    }
+
+    private Claims obterClaism( String token ) throws ExpiredJwtException {
+        return Jwts
+                .parser() // serve para decodificar o token
+                .setSigningKey(chaveAssinatura)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public boolean tokenValido( String token ){
+        try {
+            Claims claims = obterClaism(token);
+            Date dataExpiracao = claims.getExpiration();
+
+            LocalDateTime localTime = dataExpiracao
+                    .toInstant()
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDateTime();
+
+            return !LocalDateTime.now().isAfter(localTime);
+
+        } catch (Exception e){
+            return false;
+        }
+    }
+
+    public String obterLoginUsuario (String token) throws ExpiredJwtException {
+        return (String) obterClaism(token).getSubject();
     }
 }
